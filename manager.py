f1fb6f34291a18fa181782f8ba2a3697f6bad06a
danfengxu <7888376+danfengxu@user.noreply.gitee.com>
2024-01-09 11:15:10 +0800

    管理员端

---
+++ manager.py
@@ -0,0 +1,34 @@
+class manager():
+    def __init__(self):
+        pass
+
+    def login(self):
+        pass
+
+    def addporduct(self):
+        '''
+        增加商品
+        :return:
+        '''
+        pass
+
+    def delproduct(self):
+        '''
+        删除商品
+        :return:
+        '''
+        pass
+
+    def chaproduct(self):
+        '''
+        修改商品
+        :return:
+        '''
+        pass
+
+    def showproduct(self):
+        '''
+        显示商品
+        :return:
+        '''
+        pass
